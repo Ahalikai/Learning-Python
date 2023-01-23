bb31262ee93967403542651fb07cf1ac9ba1abd4
Ahalikai <974260393@qq.com>
2023-01-23 22:41:46 +0800

    2023_01_25 OpenCV_start

---
+++ OpenCV_learn.py
@@ -0,0 +1,91 @@
+
+# https://www.bilibili.com/video/BV1eR4y1R7PP/
+# 【唐博士带你学AI】唐宇迪 OpenCV
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+def cv_show(name, img):
+    cv2.imshow(name, img)
+    cv2.waitKey(0)
+
+def img_color(img, color = 'o'):
+    if color == 'o':
+        return img
+    cur_img = img.copy()
+    color_item = ['B', 'G', 'R']
+    for i in range(3):
+        if color != color_item[i]:
+            cur_img[:, :, i]= 0
+    return cur_img
+
+img = cv2.imread("cat.jpg")
+
+mit 9038ea584b434434cc2558ed802e7243d5596c00
+Author: Ahalikai <974260393@qq.com>
+Date:   Wed Jan 25 21:59:58 2023 +0800
+
+    2023_01_25 OpenCV_start
+
+commit 4d2007950bfc8fefa28b2274d05d13cfe4c35ab6
+Author: Ahalikai <974260393@qq.com>
+Date:   Wed Jan 25 21:14:35 2023 +0800
+
+    2023_01_25 OpenCV_start
+
+commit b7f4294aed5c101b3d26f3f26ead2193d541327c (origin/master)
+
+#4 filling
+top_size, bottom_size, left_size, right_size = (50, 50, 50, 50)
+
+replicate = cv2.copyMakeBorder(img, top_size, bottom_size, left_size, right_size, borderType = cv2.BORDER_REPLICATE)
+
+plt.subplots(231), plt.imshow(img, 'gray'), plt.title('original')
+plt.subplots(232), plt.imshow(replicate, 'gray'), plt.title('replicate')
+#plt.subplots(1), plt.imshow(img, 'gray'), plt.title('original')
+#plt.subplots(1), plt.imshow(img, 'gray'), plt.title('original')
+#plt.subplots(1), plt.imshow(img, 'gray'), plt.title('original')
+#plt.subplots(1), plt.imshow(img, 'gray'), plt.title('original')
+plt.show()
+
+#3 ROI
+b, g, r = cv2.split(img)
+print(b.shape)
+
+img = img[0:200, 0:200]
+#('img_200', img)
+
+cv_show('img_red', img_color(img, 'G'))
+
+#2 video
+vc = cv2.VideoCapture('1.mp4')
+
+if vc.isOpened():
+    open_vc, frame = vc.read()
+else:
+    open_vc = False
+
+while open_vc:
+    ret, frame = vc.read()
+    if frame is None:
+        break
+    if ret == True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #cv2.imshow('result', gray)
+        if cv2.waitKey(2) & 0xFF == 27:
+            break
+
+vc.release()
+cv2.destroyAllWindows()
+
+#1 img
+print(img.shape)
+
+img = cv2.imread("1_1.jpg", cv2.IMREAD_GRAYSCALE)
+
+cv_show('1_1.jpg', img)
+cv2.imwrite('1_gray.png', img)
+
+
+
--- opencv_learn.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import matplotlib as plt
-import numpy as np
-
-img = cv2.imread("1_1.jpg")
-
-cv2.imshow("1_1.jpg", img)
-cv2.waitKey(0)
