aac9ca43d7d13cf306602899c8b4a224b1ad1097
Ahalikai <974260393@qq.com>
2023-02-15 15:05:21 +0800

    2023_02_15 ChatGPT-API.py

---
+++ ChatGPT-API.py
@@ -0,0 +1,21 @@
+
+
+def regpt():
+    import openai
+    openai.api_key = 'Key'
+
+    response = openai.Completion.create(
+        model='text-davinci-003',
+        prompt=input("请输入你的问题:"),
+        temperature=1,
+        max_tokens=4000,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
+
+    print(response.choices[0].text)
+
+if __name__ == '__main__':
+    for x in range(10000):
+        regpt()
