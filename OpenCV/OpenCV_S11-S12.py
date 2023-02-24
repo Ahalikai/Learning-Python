62ec15c5564e1a8bb589b337ddb630974f130297
Ahalikai <974260393@qq.com>
2023-02-24 15:34:24 +0800

    2023_02_24 OpenCV_S11-S12.py cornerHarris&sift

--- OpenCV/OpenCV_S11-S12.py
+++ OpenCV/OpenCV_S11-S12.py
@@ -7,6 +7,17 @@ img = cv2.imread("images/ocr_a_reference.png")
 print("img shape: ", img.shape)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+sift = cv2.SIFT_create()
+kp = sift.detect(gray, None)
+
+img = cv2.drawKeypoints(gray, kp, img)
+cv2.imshow("result", img)
+cv2.waitKey(0)
+
+kp, des = sift.compute(gray, kp)
+
+# cornerHarris
+
 dst = cv2.cornerHarris(gray, 2, 3, 0.04)
 print("dst shape: ", dst.shape)
 
