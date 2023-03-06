8eba3aee4b1c2518a1b684b19f47244d67c53221
Ahalikai <974260393@qq.com>
2023-03-06 14:27:58 +0800

    2023_03_06 flower.py load_dataset

--- PyTorch/flower.py
+++ PyTorch/flower.py
@@ -22,5 +22,29 @@ train_dir = data_dir + '/train'
 test_dir = data_dir + '/valid'
 
 data_transforms = {
-    'train' : 1
+    'train' :
+        transforms.Compose([
+            transforms.Resize([96, 96]), # resize
+            transforms.RandomRotation(45), # rotation
+            transforms.CenterCrop(64), # 中心裁剪
+            transforms.RandomHorizontalFlip(p = 0.5), # 水平旋转 P概率
+            transforms.RandomVerticalFlip(p = 0.5), # 垂直旋转 P概率
+            transforms.ColorJitter(brightness=0.2, contrast=0.1, saturation=0.1, hue=0.1), #亮度、对比度、饱和度、色相
+            transforms.RandomGrayscale(p = 0.025), # 概率转化为灰度
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])#均值，标准差
+        ]),
+    'valid' :
+        transforms.Compose({
+            transforms.Resize([64, 64]),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        }),
 }
+
+batch_size = 8
+image_datasets = {x : datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'valid']}
+dataloaders = {x : torch.utils.data.Dataloader(image_datasets[x], batch_size = batch_size, shuffle = True) for x in ['train', 'valid']}
+dataset_sizes = {x : len(image_datasets[x]) for x in ['train', 'valid']}
+class_names = image_datasets['train'].classes
+
