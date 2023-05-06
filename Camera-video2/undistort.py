751b977ef165c940b5c7ca02c8cb2ed5e0f03a81
Ahalikai <974260393@qq.com>
2023-05-06 16:57:52 +0800

    2023_05_06 畸变矫正测试

--- Camera-video2/undistort.py
+++ Camera-video2/undistort.py
@@ -10,12 +10,12 @@ def undistort(frame):
  [  0.       ,  409.20308599 ,244.73617469],
  [  0.       ,    0.       ,    1.        ]])
 
-	k = np.array([[1.31688101e+03, 0.00000000e+00, 9.66128276e+02],
- [0.00000000e+00,1.31720898e+03,5.34038509e+02],
- [0.00000000e+00,0.00000000e+00,1.00000000e+00]])
+	k = np.array([[1184.52534766571, 0, 974.429294015024],
+				  [0., 1184.13524856527,475.933442967284],
+				  [0., 0., 1.]])
 
 	# d=np.array([-0.33880708 , 0.16416173 ,-0.00039069 ,-0.00056267 ,-0.056967  ])
-	d = np.array([-3.56536017e-01, 2.12249787e-01,-1.24758904e-04, 9.01910398e-04,-9.52484138e-02 ])
+	d = np.array([-0.293031197659429, 0.0625952472919022,0,0,0 ])
 	h,w=frame.shape[:2]
 	mapx,mapy=cv.initUndistortRectifyMap(k,d,None,k,(w,h),5)
 	return cv.remap(frame,mapx,mapy,cv.INTER_LINEAR)
@@ -31,12 +31,12 @@ def undistort(frame):
 #
 # cap.release()
 
-images = glob.glob('04-23camera/*.jpg')  #   拍摄的十几张棋盘图片所在目录
+images = glob.glob('E:/Matrix/262camera/32/*.jpg')  #   拍摄的十几张棋盘图片所在目录
 for fname in images:
 	print(fname)
 	img = cv.imread(fname)
-	target_img = cv.resize(img, (1920, 1080))
+	#target_img = cv.resize(img, (1280, 960))
 	#cv.imshow('img', img)
-	cv.imshow('img', undistort(target_img))
+	cv.imshow('img', undistort(img))
 	cv.waitKey(0)
 cv.destroyAllWindows()
